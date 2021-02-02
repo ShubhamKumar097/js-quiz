1acc4bf75a9491a04ce40221af2f4cb0c8c6d350
ShubhamKumar097 <69801591+ShubhamKumar097@users.noreply.github.com>
2021-02-02 19:57:54 +0530

    initial commit

---
+++ ques.js
@@ -0,0 +1,32 @@
+const quiz = [
+  {
+    q: 'Who is known as the Missile Man of India ?',
+    options:['CV Raman','APJ Abdul Kalam','S.Ramanujan','Rabindranath Tagore',],
+    answer:1
+  },
+  {
+    q: 'Which month has maximum 29 days in a leap year ?',
+    options:['January','October','December','February'],
+    answer:3
+  },
+  {
+    q: 'Which is 2*2 ?',
+    options:['2','4','44','8'],
+    answer:1
+  },
+  {
+    q:"If You have a bowl with six apples and if you take four apple out, how many do you have ?",
+    options: ["Four", "Two", "Six", "None"],
+    answer: 1
+  },
+  {
+    q: 'Which is a fruit ?',
+    options:['Banana','Car','Truck','Horse'],
+    answer:0
+  },
+  {
+    q: 'Which month comes after November ?',
+    options:['January','October','December','June'],
+    answer:2,
+  },
+];
